c497bb47491560bb258ed33458b5437ead935ac7
arnab4477 <arnabsahareal@gmail.com>
2023-02-23 14:25:03 +0530

    Created helper functions to generate pdfs from JSON data

---
+++ server/src/pdf.js
@@ -0,0 +1,45 @@
+import json2html from 'node-json2html';
+import * as pdf from 'html-pdf';
+
+const convertToHtml = (name, state, city, major, zip) => {
+  const data = { name, city, major, zip, state };
+
+  let html = json2html.transform([data], {
+    '<>': 'div',
+    style: 'text-align: center;',
+    html: [
+      { '<>': 'div', text: 'Name: ${name}' },
+      { '<>': 'div', text: 'State: ${state}' },
+      { '<>': 'div', text: 'City: ${city}' },
+      { '<>': 'div', text: 'Major: ${major}' },
+      { '<>': 'div', text: 'Zip: ${zip}' },
+    ],
+  });
+
+  return html;
+};
+
+const convertToPdf = (name, state, city, major, zip, output) => {
+  const html = convertToHtml(name, state, city, major, zip);
+
+  return new Promise((resolve, reject) => {
+    pdf
+      .create(html, {
+        childProcessOptions: {
+          env: {
+            OPENSSL_CONF: '/dev/null',
+          },
+        },
+        format: 'A4',
+      })
+      .toFile(output, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+  });
+};
+
+export default convertToPdf;
